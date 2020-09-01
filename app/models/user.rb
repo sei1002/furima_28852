4c170b278a60a10b6ee4dba2cf578735a54e33a1
sei1002 <sei1002210@gmail.com>
2020-09-01 15:32:35 +0900

    userモデルテストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :kana_first, :kana_last, :birthday, :nickname, presence: true
-  validates :password, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./, message: "Password Include both letters and numbers" }
+  validates :password, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./, message: " Include both letters and numbers" }
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
   validates :kana_first, :kana_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
 end

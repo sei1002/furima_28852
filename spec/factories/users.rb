ee1c00e7815abf1ba2a9d4f0a74b2e2874c36059
sei1002 <sei1002210@gmail.com>
2020-09-01 13:00:17 +0900

    rspec導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { "セイ" }
+    email                 { "aaaa@gmail.com" }
+    password              { "aaaa111" }
+    password_confirmation { "aaaa111" }
+    first_name            { "成" }
+    last_name             { "中田" }
+    kana_last             { "ナカタ" }
+    kana_first            { "セイ" }
+    birthday              { Faker::Date.birthday }
+  end
+end

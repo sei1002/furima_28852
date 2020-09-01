c2eb355d8ad4a8b9121f11971efd0fae1ba8f4f8
sei1002 <sei1002210@gmail.com>
2020-09-01 21:24:39 +0900

    ActiveHashの連携

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveHash::Base
-  self.date = [
+  self.data = [
     { id: 0,  name: '--' },
     { id: 1,  name: 'レディース' },
     { id: 2,  name: 'メンズ' },
--- app/models/delv_fee.rb
+++ app/models/delv_fee.rb
@@ -1,5 +1,5 @@
 class DelvFee < ActiveHash::Base
-  self.date = [
+  self.data = [
     { id: 0,  name: '--' },
     { id: 1,  name: '着払い(購入者負担)' },
     { id: 2,  name: '送料込み(出品者負担)' },

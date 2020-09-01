3ab520310cd04f92a03b46475000b95d9b2102cd
sei1002 <sei1002210@gmail.com>
2020-09-01 21:30:00 +0900

    Active_Storage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :condition, :delv_fee, :delv_time, :prefecture
+  has_one_attached :image
 end

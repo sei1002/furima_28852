23cf86f9f4446b07c4830115817cb6535ae44b25
sei1002 <sei1002210@gmail.com>
2020-09-01 11:55:52 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :first_name, :last_name, :kana_first, :kana_last, :birthday, :nickname, presence: true
+  validates :first_name, :last_name, :kana_first, :kana_last, :birthday, :nickname, presence: true
+  validates :password, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./, message: "Password Include both letters and numbers" }
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :kana_first, :kana_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
 end

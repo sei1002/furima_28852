5f78cb64b6cc56779f5cfa93004afe119ec7256e
sei1002 <sei1002210@gmail.com>
2020-09-08 11:24:05 +0900

    テストコード

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,16 @@
 class OrdersController < ApplicationController
-
   def index
-
     @order = OrderAddress.new
 
-    if user_signed_in? 
+    if user_signed_in?
       @item = Item.find(params[:item_id])
     else
       redirect_to user_session_path
     end
 
-    if user_signed_in? && current_user.id == @item.user_id 
-      redirect_to root_path
-    end
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
 
-    if @item.order
-      redirect_to root_path
-    end
-    
+    redirect_to root_path if @item.order
   end
 
   def create
@@ -26,7 +19,7 @@ class OrdersController < ApplicationController
     if @order.valid?
       pay_item
       @order.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render :index
     end
@@ -35,17 +28,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit( :postal_code, :prefecture_id, :city, :address, :build_name, :tel, :order_id).merge(user_id: current_user.id, order_id: params[:order_id], item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :build_name, :tel, :order_id).merge(user_id: current_user.id, order_id: params[:order_id], item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    
-    Payjp.api_key = "sk_test_4107de1a6af9583770a0d02b"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = 'sk_test_4107de1a6af9583770a0d02b'  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency:'jpy'                 # 通貨の種類(日本円)
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'                 # 通貨の種類(日本円)
     )
   end
- 
 end
--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,12 +1,11 @@
 class OrderAddress
-
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :build_name, :tel, :order_id, :user_id, :item_id
 
   validates :token, presence: true
-  validates :postal_code, :prefecture_id, :city, :address, :tel, presence: true 
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :tel, format: {with: /\A\d{10,11}\z/}
+  validates :postal_code, :city, :address, :tel, presence: true
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  validates :tel, format: { with: /\A\d{10,11}\z/ }
   validates :prefecture_id, numericality: { other_than: 0 }
 
   def save
@@ -14,5 +13,4 @@ class OrderAddress
 
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, build_name: build_name, tel: tel, order_id: order_id.id)
   end
-
 end

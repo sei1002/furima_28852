434e86eaf1633615ffed34a565824171390d03ef
sei1002 <sei1002210@gmail.com>
2020-09-08 13:01:23 +0900

    バリデーション修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,9 +2,11 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :build_name, :tel, :order_id, :user_id, :item_id
 
-  validates :token, :postal_code, :city, :address, :tel, presence: true
+  with_options presence: true do
+  validates :token, :city, :address
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
   validates :tel, format: { with: /\A\d{10,11}\z/ }
+  end
   validates :prefecture_id, numericality: { other_than: 0 }
 
   def save
